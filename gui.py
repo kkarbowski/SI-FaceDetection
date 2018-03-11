67f85a472a9da1820d8d94df4e79f3d2e928cbe5
Korneliusz <adis1450@gmail.com>
2018-03-11 23:38:12 +0100

    1 version Gui

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 import cv2
 
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QDialog
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QDialog, QFileDialog
 from PyQt5.QtCore import QSize, QPoint, QTimer   
 from PyQt5.QtGui import QImage, QPainter, QPixmap
 
 class MainWindow(QMainWindow):
+    BUTTON_X_POS = 30
+    TEXT_BOX_HEIGHT = 24
     def __init__(self):
         QMainWindow.__init__(self)
         self.setObjectName("Face Swaping Application")
@@ -13,90 +15,113 @@ class MainWindow(QMainWindow):
         self._central_widget = QtWidgets.QWidget(self)
         self._central_widget.setObjectName("_central_widget")
         
-
+        #video widget
         self._video = QtWidgets.QLabel(self._central_widget)
-        self._video.setGeometry(QtCore.QRect(460, 60, 551, 371))
+        self._video.setGeometry(QtCore.QRect(460, 60, 555, 370))
         self._video.setObjectName("video")
         self._capturing = Capture(self._video)
 
-        self._push_button = QtWidgets.QPushButton(self._central_widget)
-        self._push_button.setGeometry(QtCore.QRect(30, 60, 151, 31))
-        self._push_button.setObjectName("_push_button")
-        self._push_button.clicked.connect(self.handle_button)
+        #video frame rate
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.show_frame)
+        self.timer.start(60) #10 fps
+
+        #choose file button
+        self._choose_file_button = QtWidgets.QPushButton(self._central_widget)
+        self._choose_file_button.setGeometry(QtCore.QRect(30, self.BUTTON_X_POS, 151, 31))
+        self._choose_file_button.setObjectName("Input_File")
+        self._choose_file_button.clicked.connect(self.get_file)
        
-        self._font_combo_box = QtWidgets.QFontComboBox(self._central_widget)
-        self._font_combo_box.setGeometry(QtCore.QRect(20, 160, 381, 22))
-        self._font_combo_box.setObjectName("_font_combo_box")
+        #start button
+        self._start_button = QtWidgets.QPushButton(self._central_widget)
+        self._start_button.setGeometry(QtCore.QRect(240, self.BUTTON_X_POS, 151, 31))
+        self._start_button.setObjectName("_push_button_2")
+
+        #file input path (text box)
+        self._input_box = QtWidgets.QTextEdit(self._central_widget)
+        self._input_box.setGeometry(QtCore.QRect(20, 100, 381, self.TEXT_BOX_HEIGHT))
+        self._input_box.setObjectName("Input File")
+
+        #input file label
+        self._input_label = QtWidgets.QLabel(self._central_widget)
+        self._input_label.setGeometry(QtCore.QRect(20, 70, 381, self.TEXT_BOX_HEIGHT))
+        self._input_label.setObjectName("_input_label")
+        self._input_label.setText("Input file path:")
+
+        #text label "Choose a method"
+        self._method_label = QtWidgets.QLabel(self._central_widget)
+        self._method_label.setGeometry(QtCore.QRect(20, 130, 381, self.TEXT_BOX_HEIGHT))
+        self._method_label.setObjectName("_method_label")
+        self._method_label.setText("Choose a method of face detection:")
         
-        self._text_edit = QtWidgets.QTextEdit(self._central_widget)
-        self._text_edit.setGeometry(QtCore.QRect(20, 100, 381, 21))
-        self._text_edit.setObjectName("_text_edit")
-        
-        self._plain__text_edit = QtWidgets.QPlainTextEdit(self._central_widget)
-        self._plain__text_edit.setGeometry(QtCore.QRect(20, 130, 381, 21))
-        self._plain__text_edit.setObjectName("_plain__text_edit")
-        
+        #choose method
+        self._method_box = QtWidgets.QComboBox(self._central_widget)
+        self._method_box.setGeometry(QtCore.QRect(20, 160, 381, self.TEXT_BOX_HEIGHT))
+        self._method_box.setObjectName("_font_combo_box")
+        self._method_box.addItems(["1 met", "2 met", "3 met", "4 met"])
+
+        # infos about FR
         self._text_browser = QtWidgets.QTextBrowser(self._central_widget)
         self._text_browser.setGeometry(QtCore.QRect(20, 220, 381, 221))
         self._text_browser.setObjectName("_text_browser")
+        self._text_browser.setText("Adsasdasd ads asd asd asd as das das d")
         
-        self._plain__text_edit_2 = QtWidgets.QPlainTextEdit(self._central_widget)
-        self._plain__text_edit_2.setGeometry(QtCore.QRect(20, 190, 381, 21))
-        self._plain__text_edit_2.setObjectName("_plain__text_edit_2")
-       
-        self._widget = QtWidgets.QWidget(self._central_widget)
-        self._widget.setGeometry(QtCore.QRect(50, 510, 111, 111))
-        self._widget.setObjectName("_widget")
-        self._widget_2 = QtWidgets.QWidget(self._central_widget)
-        self._widget_2.setGeometry(QtCore.QRect(180, 510, 111, 111))
-        self._widget_2.setObjectName("_widget_2")
-        self._widget_3 = QtWidgets.QWidget(self._central_widget)
-        self._widget_3.setGeometry(QtCore.QRect(320, 510, 111, 111))
-        self._widget_3.setObjectName("_widget_3")
-        self._widget_4 = QtWidgets.QWidget(self._central_widget)
-        self._widget_4.setGeometry(QtCore.QRect(460, 510, 111, 111))
-        self._widget_4.setObjectName("_widget_4")
-        self._widget_5 = QtWidgets.QWidget(self._central_widget)
-        self._widget_5.setGeometry(QtCore.QRect(600, 510, 111, 111))
-        self._widget_5.setObjectName("_widget_5")
-        self._widget_6 = QtWidgets.QWidget(self._central_widget)
-        self._widget_6.setGeometry(QtCore.QRect(740, 510, 111, 111))
-        self._widget_6.setObjectName("_widget_6")
-        self._widget_7 = QtWidgets.QWidget(self._central_widget)
-        self._widget_7.setGeometry(QtCore.QRect(880, 510, 111, 111))
-        self._widget_7.setObjectName("_widget_7")
-        
+        #label infos about process of FD
+        self._info_label = QtWidgets.QLabel(self._central_widget)
+        self._info_label.setGeometry(QtCore.QRect(20, 190, 381, self.TEXT_BOX_HEIGHT))
+        self._info_label.setObjectName("_info_label")
+        self._info_label.setText("Information about face detection:")
+
+        # on/off face swap checkbutton
         self._check_box = QtWidgets.QCheckBox(self._central_widget)
-        self._check_box.setGeometry(QtCore.QRect(460, 440, 121, 21))
+        self._check_box.setGeometry(QtCore.QRect(460, 440, 150, self.TEXT_BOX_HEIGHT))
         self._check_box.setObjectName("_check_box")
-        
-        self._push_button_2 = QtWidgets.QPushButton(self._central_widget)
-        self._push_button_2.setGeometry(QtCore.QRect(240, 60, 151, 31))
-        self._push_button_2.setObjectName("_push_button_2")
-        
+
+
+        pic_face=QPixmap("./bach.jpg")
+        pic_face=pic_face.scaled(110, 110)
+
+        self._face_slot = []
+        for i in range(0,14):
+            self._face_slot.append(QtWidgets.QLabel(self._central_widget))
+            if i < 7:
+                self._face_slot[i].setGeometry(QtCore.QRect(50 + i*130, 510, 111, 111))
+            else:
+                self._face_slot[i].setGeometry(QtCore.QRect(50 + (i-7)*130, 630, 111, 111))
+            self._face_slot[i].setObjectName("_face_slot_"+str(i))
+            self._face_slot[i].setPixmap(pic_face)
+            self._face_slot[i].mousePressEvent = self.select_method
+              
         self.setCentralWidget(self._central_widget)
         
-        self._menubar = QtWidgets.QMenuBar(self)
-        self._menubar.setGeometry(QtCore.QRect(0, 0, 1038, 21))
-        self._menubar.setObjectName("_menubar")
-        
-        self.setMenuBar(self._menubar)
-        self._statusbar = QtWidgets.QStatusBar(self)
-        self._statusbar.setObjectName("_statusbar")
-        self.setStatusBar(self._statusbar)
+        #self._menubar = QtWidgets.QMenuBar(self)
+        #self._menubar.setGeometry(QtCore.QRect(0, 0, 1038, 21))
+        #self._menubar.setObjectName("_menubar") 
+        #self.setMenuBar(self._menubar)
+
+        #self._statusbar = QtWidgets.QStatusBar(self)
+        #self._statusbar.setObjectName("_statusbar")
+        #self.setStatusBar(self._statusbar)
 
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def handle_button(self):
+    def show_frame(self):
         self._capturing.capture()
+
+    def get_file(self):
+        self._file_path = QFileDialog.getOpenFileName(self, 'Choose file','c:\\',"Image files (*.jpg *.gif)")
+        self._input_box.setText(self._file_path[0])
+
+    def select_method(self, event):
+        print("lol xd")
 
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self._push_button.setText(_translate("MainWindow", "PushButton"))
-        self._check_box.setText(_translate("MainWindow", "CheckBox"))
-        self._push_button_2.setText(_translate("MainWindow", "PushButton"))
+        self.setWindowTitle(_translate("MainWindow", "Face Swaping Application"))
+        self._choose_file_button.setText(_translate("MainWindow", "Choose input File"))
+        self._check_box.setText(_translate("MainWindow", "Turn off face swaping"))
+        self._start_button.setText(_translate("MainWindow", "Start"))
 
 
 
@@ -113,7 +138,6 @@ class Capture():
         qimg = QtGui.QImage(cvRGBImg.data,cvRGBImg.shape[1], cvRGBImg.shape[0], QtGui.QImage.Format_RGB888)
         qpm = QtGui.QPixmap.fromImage(qimg)
         self._video_elem.setPixmap(qpm)
-        cv2.waitKey(5)
 
 
 
--- program.py
+++ program.py
@@ -1,12 +1,15 @@
 from gui import *
 import sys
-import threading
 
 
 class Program():
     def __init__(self):
         self._app = QtWidgets.QApplication(sys.argv)
         self._main_win = MainWindow()
+
+    def begin(self):
         self._main_win.show()
         sys.exit( self._app.exec_() )
+
+
 
