28ae315770e20d30b06a74590922e10cebe570cd
Mateusz <wszeborowskimateusz@gmail.com>
2018-03-28 19:02:51 +0200

    Bug fix. Now swapped face has normal colors instead of blue.

--- face_swap.py
+++ face_swap.py
@@ -52,6 +52,7 @@ class Tracker:
                                                 int(tracked_position.height()))
 
             frame = swap_faces(frame, destination_face_area, self._source_img, self._source_face_area)
+            #frame = face_swap(frame, destination_face_area, self._source_img, self._source_face_area)
 
         return frame
 
@@ -116,7 +117,8 @@ class TrackerOpenCV:
 
 def swap_faces(destination_img, destination_face_area, source_img, source_face_area):
     # crop the source face
-    crop_source_img = source_img[source_face_area.y:source_face_area.y + source_face_area.h,
+    source_img_tmp = cv2.cvtColor(source_img, cv2.COLOR_RGB2BGR)
+    crop_source_img = source_img_tmp[source_face_area.y:source_face_area.y + source_face_area.h,
                                  source_face_area.x:source_face_area.x + source_face_area.w]
     # resize and blend the face to be swapped in
     face = cv2.resize(crop_source_img, (destination_face_area.w, destination_face_area.h),
@@ -131,3 +133,172 @@ def swap_faces(destination_img, destination_face_area, source_img, source_face_a
     swapped_img[destination_face_area.y:destination_face_area.y + destination_face_area.h,
                 destination_face_area.x:destination_face_area.x + destination_face_area.w] = face
     return swapped_img
+
+'''
+PREDICTOR_PATH = "predictor/shape_predictor_68_face_landmarks.dat"
+FEATHER_AMOUNT = 11
+
+FACE_POINTS = list(range(17, 68))
+MOUTH_POINTS = list(range(48, 61))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+NOSE_POINTS = list(range(27, 35))
+JAW_POINTS = list(range(0, 17))
+
+# Points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
+                RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+
+# Points from the second image to overlay on the first. The convex hull of each
+# element will be overlaid.
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+
+# Amount of blur to use during colour correction, as a fraction of the
+# pupillary distance.
+COLOUR_CORRECT_BLUR_FRAC = 0.6
+
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+detector = dlib.get_frontal_face_detector()
+
+
+def get_landmarks(im, im_face_area):
+    rect = dlib.rectangle(im_face_area.x,  im_face_area.y, im_face_area.x+im_face_area.w,
+                          im_face_area.y+im_face_area.h)
+
+    return np.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+
+def draw_convex_hull(im, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+
+
+def get_face_mask(im, landmarks):
+    im = np.zeros(im.shape[:2], dtype=np.float64)
+
+    for group in OVERLAY_POINTS:
+        draw_convex_hull(im,
+                         landmarks[group],
+                         color=1)
+
+    im = np.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
+
+
+def transformation_from_points(points1, points2):
+    """
+    Return an affine transformation [s * R | T] such that:
+        sum ||s*R*p1,i + T - p2,i||^2
+    is minimized.
+    """
+    # Solve the procrustes problem by subtracting centroids, scaling by the
+    # standard deviation, and then using the SVD to calculate the rotation. See
+    # the following for more details:
+    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
+
+    points1 = points1.astype(np.float64)
+    points2 = points2.astype(np.float64)
+
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
+    points1 -= c1
+    points2 -= c2
+
+    s1 = np.std(points1)
+    s2 = np.std(points2)
+    points1 /= s1
+    points2 /= s2
+
+    U, S, Vt = np.linalg.svd(points1.T * points2)
+
+    # The R we seek is in fact the transpose of the one given by U * Vt. This
+    # is because the above formulation assumes the matrix goes on the right
+    # (with row vectors) where as our solution requires the matrix to be on the
+    # left (with column vectors).
+    R = (U * Vt).T
+
+    return np.vstack([np.hstack(((s2 / s1) * R,
+                                       c2.T - (s2 / s1) * R * c1.T)),
+                         np.matrix([0., 0., 1.])])
+
+
+def read_im_and_landmarks(img, img_face_area):
+    im = img.copy()
+
+    s = get_landmarks(im, img_face_area)
+
+    return im, s
+
+
+def warp_im(im, M, dshape):
+    output_im = np.zeros(dshape, dtype=im.dtype)
+    cv2.warpAffine(im,
+                   M[:2],
+                   (dshape[1], dshape[0]),
+                   dst=output_im,
+                   borderMode=cv2.BORDER_TRANSPARENT,
+                   flags=cv2.WARP_INVERSE_MAP)
+    return output_im
+
+
+def correct_colours(im1, im2, landmarks1):
+    blur_amount = COLOUR_CORRECT_BLUR_FRAC * np.linalg.norm(
+        np.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
+        np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+    blur_amount = int(blur_amount)
+    if blur_amount % 2 == 0:
+        blur_amount += 1
+    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
+    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
+
+    # Avoid divide-by-zero errors.
+    im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
+
+    return (im2.astype(np.float64) * im1_blur.astype(np.float64) /
+            im2_blur.astype(np.float64))
+
+
+def face_swap(destination_img, destination_face_area, source_img, source_face_area):
+
+    im1, landmarks1 = read_im_and_landmarks(destination_img, destination_face_area)
+    im2, landmarks2 = read_im_and_landmarks(source_img, source_face_area)
+    im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
+    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
+
+    M = transformation_from_points(landmarks1[ALIGN_POINTS],
+                                   landmarks2[ALIGN_POINTS])
+
+    mask = get_face_mask(im2, landmarks2)
+
+    warped_mask = warp_im(mask, M, im1.shape)
+    combined_mask = np.max([get_face_mask(im1, landmarks1), warped_mask], axis=0)
+
+    warped_im2 = warp_im(im2, M, im1.shape)
+    warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
+
+    output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+
+    return output_im
+
+'''
